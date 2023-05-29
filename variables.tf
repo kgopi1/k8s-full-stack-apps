cfbe671d5f2ed866f5e7dae9bbecec4a4bffd597
Gopi K <k.gopi1@gmail.com>
2023-05-29 17:39:55 +0100

    added resource group

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  type        = string
+  default     = "testrg"
+  description = "Resource Group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Location"
+
+}
